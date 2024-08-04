c2ac0785ca504ab31753b44d61047645d0ed0cf3
bldambtn <166319659+bldambtn@users.noreply.github.com>
2024-08-04 13:21:43 -0500

    added ac for 1,2, and 5

--- db/schema.sql
+++ db/schema.sql
@@ -6,22 +6,23 @@ CREATE DATABASE employee_db;
 -- Connect to the newly created database
 \c employee_db;
 
-DROP TABLE IF EXISTS department;
-DROP TABLE IF EXISTS role;
 DROP TABLE IF EXISTS employee;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS department;
 
 -- Create a department table
 CREATE TABLE department (
     id SERIAL PRIMARY KEY,
-    department_name VARCHAR(30) UNIQUE NOT NULL
+    name VARCHAR(30) UNIQUE NOT NULL
 );
 
 -- Create a role table
-CREATE TABLE role (
+CREATE TABLE roles (
     id SERIAL PRIMARY KEY,
     title VARCHAR(30) UNIQUE NOT NULL,
     salary DECIMAL NOT NULL,
-    FOREIGN KEY (department_id) REFERENCES department(id) INTEGER NOT NULL
+    department_id INTEGER NOT NULL,
+    FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 -- Create an employee table
@@ -29,7 +30,8 @@ CREATE TABLE employee (
     id SERIAL PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    FOREIGN KEY (role_id) REFERENCES role(id) INTEGER NOT NULL,
-    --manager_id: integer (hold reference to another employee that is the manager of current employee, null if employee has no manager)
-    --PRIMARY KEY (manager_id) REFERENCES employee(id) INTEGER NOT NULL
+    role_id INTEGER NOT NULL,
+    manager_id INTEGER,
+    FOREIGN KEY (role_id) REFERENCES roles(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
